a62766af97f12b1df6b8e26d50607ac01ca565d3
bengeos <george.beng@gmail.com>
2018-07-27 11:52:06 +0300

    updates on root url for production and dev

--- src/app/components/common/layouts/layouts.module.ts
+++ src/app/components/common/layouts/layouts.module.ts
@@ -6,13 +6,11 @@ import {BsDropdownModule} from 'ngx-bootstrap';
 
 import {BasicLayoutComponent} from "./basicLayout.component";
 import {BlankLayoutComponent} from "./blankLayout.component";
-import {TopNavigationLayoutComponent} from "./topNavigationlayout.component";
-
-import {NavigationComponent} from "./../navigation/navigation.component";
-import {FooterComponent} from "./../footer/footer.component";
-import {TopNavbarComponent} from "./../topnavbar/topnavbar.component";
-import {TopNavigationNavbarComponent} from "./../topnavbar/topnavigationnavbar.component";
 import { TopNavigationLayoutsComponent } from './top-navigation-layouts.component';
+import {FooterComponent} from "../footer/footer.component";
+import {NavigationComponent} from "../navigation/navigation.component";
+import {TopNavbarComponent} from "../topnavbar/topnavbar.component";
+import {TopNavigationNavbarComponent} from "../topnavbar/topnavigationnavbar.component";
 
 
 @NgModule({
