7afc0b1f830b839c8cec23c43780d59b535137a2
bengeos <george.beng@gmail.com>
2018-07-27 11:53:20 +0300

    updates on root url for production and dev

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,6 @@
 import {Routes} from "@angular/router";
-
-import {Dashboard1Component} from "./views/dashboards/dashboard1.component";
-import {Dashboard2Component} from "./views/dashboards/dashboard2.component";
-import {Dashboard3Component} from "./views/dashboards/dashboard3.component";
-import {Dashboard4Component} from "./views/dashboards/dashboard4.component";
-import {Dashboard41Component} from "./views/dashboards/dashboard41.component";
-import {Dashboard5Component} from "./views/dashboards/dashboard5.component";
-
-import {StarterViewComponent} from "./views/appviews/starterview.component";
-import {LoginComponent} from "./views/appviews/login.component";
-
 import {BlankLayoutComponent} from "./components/common/layouts/blankLayout.component";
 import {BasicLayoutComponent} from "./components/common/layouts/basicLayout.component";
-import {TopNavigationLayoutComponent} from "./components/common/layouts/topNavigationlayout.component";
 
 export const ROUTES:Routes = [
   // Main redirect
